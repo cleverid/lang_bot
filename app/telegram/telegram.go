e96827105684f535072c12aaf05769a0b44869d5
e.shaposhnikov <eawapownikov@gmail.com>
2025-07-12 23:07:57 +0300

    keyboard

--- app/telegram/telegram.go
+++ app/telegram/telegram.go
@@ -13,6 +13,32 @@ type telegram struct {
 	logger loggergo.ILogger
 }
 
+var numericKeyboard = tgbotapi.NewInlineKeyboardMarkup(
+	tgbotapi.NewInlineKeyboardRow(
+		tgbotapi.NewInlineKeyboardButtonURL("1.com", "http://1.com"),
+		tgbotapi.NewInlineKeyboardButtonData("2", "2"),
+		tgbotapi.NewInlineKeyboardButtonData("3", "3"),
+	),
+	tgbotapi.NewInlineKeyboardRow(
+		tgbotapi.NewInlineKeyboardButtonData("4", "4"),
+		tgbotapi.NewInlineKeyboardButtonData("5", "5"),
+		tgbotapi.NewInlineKeyboardButtonData("6", "6"),
+	),
+)
+
+var numericKeyboard2 = tgbotapi.NewReplyKeyboard(
+	tgbotapi.NewKeyboardButtonRow(
+		tgbotapi.NewKeyboardButton("1"),
+		tgbotapi.NewKeyboardButton("2"),
+		tgbotapi.NewKeyboardButton("3"),
+	),
+	tgbotapi.NewKeyboardButtonRow(
+		tgbotapi.NewKeyboardButton("4"),
+		tgbotapi.NewKeyboardButton("5"),
+		tgbotapi.NewKeyboardButton("6"),
+	),
+)
+
 func New(config TelegramConfig, logger loggergo.ILogger) (tel types.ITelegram, err error) {
 	tel = &telegram{
 		config: config,
@@ -39,9 +65,27 @@ func (t *telegram) Start() error {
 					"text", update.Message.Text)
 
 				msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
-				msg.ReplyToMessageID = update.Message.MessageID
-
+				// msg.ReplyToMessageID = update.Message.MessageID
+				switch update.Message.Command() {
+				case "translate":
+					msg.ReplyMarkup = numericKeyboard2
+				}
+				msg.ReplyMarkup = tgbotapi.NewRemoveKeyboard(true)
+				// msg.ReplyMarkup = numericKeyboard
 				bot.Send(msg)
+			} else if update.CallbackQuery != nil {
+				// Respond to the callback query, telling Telegram to show the user
+				// a message with the data received.
+				callback := tgbotapi.NewCallback(update.CallbackQuery.ID, update.CallbackQuery.Data)
+				if _, err := bot.Request(callback); err != nil {
+					panic(err)
+				}
+
+				// // And finally, send a message containing the data received.
+				// msg := tgbotapi.NewMessage(update.CallbackQuery.Message.Chat.ID, update.CallbackQuery.Data)
+				// if _, err := bot.Send(msg); err != nil {
+				// 	panic(err)
+				// }
 			}
 		}
 	}()
--- cmd/server/server.go
+++ cmd/server/server.go
@@ -1,10 +1,19 @@
 package main
 
 import (
+	"fmt"
 	"lb/app"
 )
 
 func main() {
 	app := app.Init()
 	app.Start()
+	for _, item := range []string{} {
+		fmt.Println(item)
+	}
+	start()
+}
+
+func start() {
+	fmt.Println("")
 }
